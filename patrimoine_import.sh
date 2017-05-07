c6c3e5fcc9dfed3b9988850068c9ab08f9384a9c
Wilkins <thibault@taillandier.name>
2017-05-07 22:03:04 +0200

    Mise à jour avec le patrimoine 2016

--- patrimoine_import.sh
+++ patrimoine_import.sh
@@ -1,5 +1,9 @@
-cat patrimoine.sql | mysql -uroot -p data 
-mysqlimport --default-character-set=utf8 --ignore-line=1  --fields-optionally-enclosed-by='"'     --fields-terminated-by=';'  -uroot -p data /path/to/patrimoine.csv
+#cat patrimoine.sql | mysql -uroot -p data 
+mysqlimport  --ignore-lines=1  --fields-optionally-enclosed-by='"'     --fields-terminated-by=','  -uroot -p data /tmp/patrimoine.csv
+#mysqlimport --default-character-set=utf8 --ignore-line=1  --fields-optionally-enclosed-by='"'     --fields-terminated-by=';'  -uroot -p data /tmp/patrimoine.csv
+
+echo 'update patrimoine set coordonnees=concat(latitude, ",", longitude);' | mysql -uroot -p data 
+
 # http://whc.unesco.org/fr/list/xls/
 
 # :%s/\([^;,]\+\),\1/\1/g | %s/\([^;,]\+\),\1/\1/g | %s/\([^;,]\+\),\1/\1/g
